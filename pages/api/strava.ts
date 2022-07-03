fde86de4079f20bc92a80c5e04b54880a5b47678
Will Borysiak <will.gains@icloud.com>
2022-07-03 07:59:54 +0100

    testing with api route (again) after env fix

---
+++ pages/api/strava.ts
@@ -0,0 +1,53 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const stravaAPI = async (req: NextApiRequest, res: NextApiResponse) => {
+	// Tokens and Authentication
+	const headers = {
+		Accept: 'application/json, text/plain, */*',
+		'Content-Type': 'application/json',
+	};
+
+	const body = JSON.stringify({
+		client_id: process.env.STRAVA_CLIENT_ID,
+		client_secret: process.env.STRAVA_SECRET,
+		refresh_token: process.env.STRAVA_REFRESH_TOKEN,
+		grant_type: 'refresh_token',
+	});
+
+	const refreshAccess = await fetch('https://www.strava.com/oauth/token', {
+		method: 'post',
+		headers: headers,
+		body: body,
+	});
+
+	const accessJson = await refreshAccess.json();
+
+	// API Calls
+
+	// Stats
+	const statsRequest = await fetch(
+		'https://www.strava.com/api/v3/athletes/90122035/stats?access_token=' + accessJson.access_token,
+	);
+
+	const stats = await statsRequest.json();
+
+	// Activities
+	const activitiesRequest = await fetch(
+		'https://www.strava.com/api/v3/athlete/activities?after=1650416400&access_token=' + accessJson.access_token,
+	);
+	const activities = await activitiesRequest.json();
+
+	// Segments
+	const segmentsRequest = await fetch(
+		'https://www.strava.com/api/v3/segments/starred?access_token=' + accessJson.access_token,
+	);
+	const segments = await segmentsRequest.json();
+
+	return res.status(200).json({
+		activities,
+		stats,
+		segments,
+	});
+};
+
+export default stravaAPI;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,57 +13,10 @@ import { Strava } from '../models/strava.model';
 import Footer from '../components/layout/Footer';
 import ErrorMessage from '../components/dashboard/main/ErrorMessage';
 import Dashboard from '../components/dashboard/main/Dashboard';
-
-const stravaApi = async () => {
-	// Tokens and Authentication
-	const headers = {
-		Accept: 'application/json, text/plain, */*',
-		'Content-Type': 'application/json',
-	};
-
-	const body = JSON.stringify({
-		client_id: process.env.STRAVA_CLIENT_ID,
-		client_secret: process.env.STRAVA_SECRET,
-		refresh_token: process.env.STRAVA_REFRESH_TOKEN,
-		grant_type: 'refresh_token',
-	});
-
-	const refreshAccess = await fetch('https://www.strava.com/oauth/token', {
-		method: 'post',
-		headers: headers,
-		body: body,
-	});
-
-	const accessJson = await refreshAccess.json();
-
-	// API Calls
-
-	// Stats
-	const statsRequest = await fetch(
-		'https://www.strava.com/api/v3/athletes/90122035/stats?access_token=' + accessJson.access_token,
-	);
-
-	const stats = await statsRequest.json();
-
-	// Activities
-	const activitiesRequest = await fetch(
-		'https://www.strava.com/api/v3/athlete/activities?after=1650416400&access_token=' + accessJson.access_token,
-	);
-	const activities = await activitiesRequest.json();
-
-	// Segments
-	const segmentsRequest = await fetch(
-		'https://www.strava.com/api/v3/segments/starred?access_token=' + accessJson.access_token,
-	);
-	const segments = await segmentsRequest.json();
-
-	const data = { stats, activities, segments };
-
-	return data;
-};
+import fetcher from '../components/data/fetcher';
 
 const Home: NextPage = () => {
-	const { data, error } = useSWR<Strava>('strava', stravaApi);
+	const { data, error } = useSWR<Strava>('/api/strava', fetcher);
 
 	console.log(data);
 
